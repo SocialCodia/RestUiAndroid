09b71f1fcaeb93ea708d07aba3d928ea1ad104a7
mufazmi <info.mufazmi@gmail.com>
2023-10-21 17:53:46 +0530

    Umair Farooqui (@mufazmi) starts the coding adventure from socialcodia 🚀. Explore more at @socialcodia! 🌐

---
+++ app/src/main/java/com/socialcodia/restapi/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.socialcodia.restapi.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.socialcodia.restapi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
